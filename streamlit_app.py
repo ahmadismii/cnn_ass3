204f4271747ef1816f0a7d92fe954d8f626c7f14
ahmadismii <165884499+ahmadismii@users.noreply.github.com>
2024-04-03 19:47:20 +1100

    #2

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
 # Generate random data for the scatter plot
 np.random.seed(42)
